c01cecc86bb8e4cace901b8d0edfb96532a9e787
Matt <kritoke@gmail.com>
2016-03-20 18:54:27 +0000

    initial commit

---
+++ preliminary/basics/exercise4.rb
@@ -0,0 +1,5 @@
+# Use years from excercise3 in an array and output them using puts
+
+date = [1999, 1980, 2016, 1995]
+
+date.each {|year| puts year}
---
+++ preliminary/basics/exercise6.rb
@@ -0,0 +1,9 @@
+# Calculates the squares of 3 float numbers and outputs the result to the screen.
+
+def find_square(num)
+  num ** 2
+end
+
+puts find_square(4.544)
+puts find_square(6.564)
+puts find_square(565.543)
