2da901b28c7beae94f5d309f039c7d279c756c59
matthew.rhone <matthew.rhone@outlook.com>
2016-07-30 19:34:40 -0400

    Add two Lesson 3 Easy 2 Questions.

---
+++ course_101/lesson_3/easy2question1.rb
@@ -0,0 +1,7 @@
+# See if "Spot" is present in ages hash.
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+p ages.key?('Spot')
+p ages.include?('Spot')
+p ages.member?('Spot')
