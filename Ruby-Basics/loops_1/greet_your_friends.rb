e2c2b52e18d03b288de218c71f67e99f2f8b2c31
kritoke <matthew.rhone@outlook.com>
2016-08-07 20:37:30 +0000

    Move exercises to exercise folder.

--- Ruby-Basics/loops_1/greet_your_friends.rb
+++
@@ -1,5 +0,0 @@
-# Given the following array of names, use a for loop to greet each friend individually.
-
-friends = ['Sarah', 'John', 'Hannah', 'Dave']
-
-friends.each { |name| puts "Hello, #{name}!" }
--- Ruby-Basics/loops_2/catch_the_number.rb
+++
@@ -1,7 +0,0 @@
-#  loop stops if number is between 0 and 10.
-
-loop do
-  number = rand(100)
-  puts number
-  break if number <= 10
-end
