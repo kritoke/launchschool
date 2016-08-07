b6d8749e46ff4b5735ba1e7ba646070b6f755909
matthew.rhone <matthew.rhone@outlook.com>
2016-08-07 14:51:53 -0400

    Added more basics exercises.

---
+++ course_101/lesson_3/easy2question3.rb
@@ -0,0 +1,12 @@
+# Add up all of the ages from our current Munster family hash
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+total_age = 0
+ages.each do |_, age|
+  total_age = age + total_age
+end
+
+ages.keep_if
+
+p "Total Munster age is #{total_age}"
