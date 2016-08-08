c8eaf34e0d02798b3bcbd225d5c1892d8451541e
kritoke <matthew.rhone@outlook.com>
2016-08-08 23:47:00 +0000

    Added easy 3 answers in lesson 3.

---
+++ course_101/lesson_3/easy3question2.rb
@@ -0,0 +1,4 @@
+# add Dino to the array
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << 'Dino'
---
+++ course_101/lesson_3/easy3question3.rb
@@ -0,0 +1,5 @@
+# add multiple items to our array
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << 'Dino' << 'Hoppy'
+p flintstones
