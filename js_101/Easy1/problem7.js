4452724e0731e616f71044cec1ba7deff7cd4d2f
Matt Rhone <3081486+kritoke@users.noreply.github.com>
2023-09-04 14:29:08 -0500

    Problem 7

---
+++ js_101/Easy1/problem7.js
@@ -0,0 +1,9 @@
+function shortLongShort(string1, string2) {
+    if (string1.length < string2.length)
+    {
+        return string1 + string2 + string1;
+    }
+    else {
+        return string2 + string1 + string2;
+    }
+}
