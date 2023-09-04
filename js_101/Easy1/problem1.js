90c13d526611f1a8d8051bea12cb3acb6813627a
Matt Rhone <3081486+kritoke@users.noreply.github.com>
2023-09-04 13:08:56 -0500

    Problem1 solution.

---
+++ js_101/Easy1/problem1.js
@@ -0,0 +1,13 @@
+function isOdd(number)
+{
+    let result = number % 2;
+    return result !== 0;
+}
+
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
